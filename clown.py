58b8c3b7c2058f77abc7478c19102bc10cb07517
Greg Warner <cats-carrying-canes@canary.net>
2025-02-10 22:51:39 -0600

    send in the clown

--- clown.py
+++ clown.py
@@ -26,5 +26,27 @@ class Clown:
         self.max_acts: int = len(self.circus)-1
         self._acts: list[str] = []
 
+    def send_in_the_clown(self):
+        """
+        The clown is called onto the stage.
+            
+        He carries his act, and is placed in the _acts first.
+        Then the show commences with the first act, and the remaining acts follow.
+        """
+        print("{}.".format(self.clown.split()[-1]))
+        self._acts = [self.clown, self.circus[1]]
+        self._call_character(self.clown)
+
+    def _call_character(self, character: str) -> None:
+        """
+        The chracter's name is called.
+
+        Args:
+            character (str): the name of the character
+        """
+        punctuation = "!" if len(self._acts)+1 == self.max_acts else "."
+        print(character[0].upper()+character[1:]+punctuation) if character != self.circus[-1] else None
+
 
 clown = Clown()
+clown.send_in_the_clown()
